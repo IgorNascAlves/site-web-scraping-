76faad990586b043b32292eee3089543469c9aa1
Igor do Nascimento Alves <igor.nascimento.flipe@gmail.com>
2023-04-05 10:34:32 -0300

    pegando dados da API

--- js/index.js
+++ js/index.js
@@ -3,8 +3,25 @@ var total_count;
 var links;
 var pages;
 
-async function funcjoao() {
-    var mensagemErro = document.getElementById('cardJoao');
+async function createCard() {
+
+	try {
+        var consultaCEP = await fetch(`https://IdolizedBriefChief.popoflipe.repl.co/produtos?regiao=norte&ano=2022`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente!');
+        }
+	}
+		catch (erro) {
+			mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
+			console.log(erro);
+		}
+
+	var mensagemErro = document.getElementById('cardJoao');
+
+	var cep = consultaCEPConvertida[0];
+	console.log(cep)
+
 	mensagemErro.innerHTML = `<div class="well card">
 							<div class="col-md-3 image-card">
 								<img alt="Foto" height="155"
@@ -12,12 +29,12 @@ async function funcjoao() {
 									width="220" />
 							</div>
 							<div class="col-md-6 body-card">
-							    <p class="txt-name inline">João</p>
-							    <p class="txt-category badge badge-secondary inline">NOVO</p>
-							    <p class="txt-motor">Motor João</p>
-							    <p class="txt-description">João - João km</p>
+							    <p class="txt-name inline">${cep.Produto}</p>
+							    <p class="txt-category badge badge-secondary inline">${cep['Categoria do Produto']}</p>
+							    <p class="txt-motor">${cep['Local da compra']}</p>
+							    <p class="txt-description">${cep['Vendedor']}</p>
 							    <ul class="lst-items">
-								<li class="txt-items">► João</li>
+								<li class="txt-items">►${cep['Data da Compra']}</li>
 								<li class="txt-items">► João de João</li>
 								<li class="txt-items">► João de João</li>
 								<li class="txt-items">► João de João</li>
@@ -27,177 +44,14 @@ async function funcjoao() {
 							</div>
 							<div class="col-md-3 value-card">
 								<div class="value">
-									<p class="txt-value">R$ 999.999.999,00</p>
+									<p class="txt-value">R$ ${cep['Preço']}</p>
 								</div>
 							</div>
 						</div>`
-}
 
+	
+}
 
 var botaoJoao = document.getElementById('botaoJoao');
-botaoJoao.addEventListener("click", () => funcjoao());
 
-//Organização do código
-_$ = function (obj) {
-	return document.getElementById(obj);
-}
-
-function selectPage(val) {
-	requisitar("https://IdolizedBriefChief.popoflipe.repl.co/produtos?_page=" + val + "&_limit=10");
-}
-
-// Montando os cards de anúncios
-function createCard() {
-	// Paginação
-	response = '<ul class="pagination">';
-
-	var count = 0
-	for (var i in links) { // Solução para navegadores antigos
-		if (links.hasOwnProperty(i)) {
-			count++;
-		}
-	}
-
-	if (count == 4) {
-		response +=
-			'	<li class="page-item"><a id="novos" class="page-link" href="./index?page=' + (pages - 1) + '">&#9668;</a></li>' +
-			'	<li class="page-item"><a id="usados" class="page-link" href="./index?page=' + (pages + 1) + '">&#9658;</a></li>' +
-			'</ul>';
-	} else {
-		if (links[1].type == 'prev') {
-			response +=
-				'	<li class="page-item"><a id="novos" class="page-link" href="./index?page=' + (pages - 1) + '">&#9668;</a></li>' +
-				'	<li class="page-item"><a id="usados" class="page-link">&#9658;</a></li>' +
-				'</ul>';
-		} else {
-			response +=
-				'	<li class="page-item"><a id="novos" class="page-link">&#9668;</a></li>' +
-				'	<li class="page-item"><a id="usados" class="page-link" href="./index?page=' + (pages + 1) + '">&#9658;</a></li>' +
-				'</ul>';
-		}
-	}
-
-	$('.container-pagination').append(response);
-
-}
-
-//Função para instanciar o objeto XMLHttpResquest
-function iniciaAjax() {
-	var objetoAjax = false;
-	if (window.XMLHttpRequest) {
-		objetoAjax = new XMLHttpRequest();
-	} else if (window.ActiveXObject) {
-		try {
-			objetoAjax = new ActiveXObject("Msxml2.XMLHTTP");
-		} catch (e) {
-			try {
-				objetoAjax = new ActiveXObject("Microsoft.XMLHTTP");
-			} catch (ex) {
-				objetoAjax = false;
-			}
-		}
-	}
-	return objetoAjax;
-}
-
-//Função para requisitar um arquivo
-function requisitar(arquivo) {
-	pages = Number(getParameterByName('_page', arquivo));
-	var requisicaoAjax = iniciaAjax();
-	if (requisicaoAjax) {
-		requisicaoAjax.onreadystatechange = function () {
-			trataRespostaJSON(requisicaoAjax);
-			$('#loadingModal_content').html('Carregando...');
-			$('#loadingModal').modal('show');
-		};
-		requisicaoAjax.open("GET", arquivo, true);
-		requisicaoAjax.send(null);
-		return true;
-	} else {
-		return false;
-	}
-}
-
-//Função para tratamento de arquivo JSON
-function trataRespostaJSON(requisicaoAjax) {
-	if (requisicaoAjax.readyState == 4) {
-		if (requisicaoAjax.status == 200 || requisicaoAjax.status == 304) {
-			try {
-				dados = JSON.parse(requisicaoAjax.responseText);
-				loadModal(true);
-				resetModal();
-			} catch (e) {
-				dados = eval("(" + requisicaoAjax.responseText + ")");
-				loadModal(true);
-				resetModal();
-			}
-
-			// Obtendo o tamanho total da resposta
-			string_headers = requisicaoAjax.getAllResponseHeaders().split('\r\n');
-			var headers = string_headers.reduce(function (acc, current, i) {
-				var parts = current.split(': ');
-				acc[parts[0].toLowerCase()] = parts[1];
-				return acc;
-			}, {});
-			total_count = Number(headers['x-total-count']);
-
-			// Obtendo os links de paginação
-			for (let prop in headers) {
-				// Imprimindo a propriedade e o valor
-				console.log(prop + ": " + headers[prop]);
-			}
-			string_headers = requisicaoAjax.responseText?.split('\r\n');
-			parts = headers['link']?.split(',');
-			links = parts.reduce(function (acc, current, i) {
-				aux = current.trim().split(';');
-				acc[i] = { 'link': aux[0].replace("<", "'").replace(">", "'"), 'type': aux[1].replace(" rel=", "").replace('"', "").replace('"', "") };
-				return acc;
-			}, {})
-
-			// Criando os cards de anúncios
-			createCard();
-
-		} else {
-			alert("Problema de comunicação com os servidor. Tente mais tarde.");
-			loadModal(false);
-			resetModal();
-		}
-	}
-}
-
-// Carregamento do modal de progresso da requisição
-function loadModal(type) {
-	if (type) { // Sucesso
-		$('#loader').removeClass('loader');
-		$('#loader').addClass('glyphicon glyphicon-ok');
-		$('#loadingModal_label').html('Sucesso!');
-		$('#loadingModal_content').html('<br>Busca concluída!');
-	} else { // Fracasso
-		$('#loader').removeClass('loader');
-		$('#loader').addClass('glyphicon glyphicon-remove');
-		$('#loadingModal_label').html('Falha!');
-		$('#loadingModal_content').html('<br>Tente Novamente Mais Tarde!');
-	}
-}
-
-// Descarregamento do modal de progresso da requisição
-function resetModal() {
-	//Aguarda 2 segundos até restaurar e fechar o modal
-	setTimeout(function () {
-		$('#loader').removeClass();
-		$('#loader').addClass('loader');
-		$('#loadingModal_label').html('<span class="glyphicon glyphicon-refresh"></span>Aguarde...');
-		$('#loadingModal').modal('hide');
-	}, 500);
-}
-
-// Função para obter os parâmetros da query
-function getParameterByName(name, url) {
-	if (!url) url = window.location.href;
-	name = name.replace(/[\[\]]/g, '\\$&');
-	var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-		results = regex.exec(url);
-	if (!results) return null;
-	if (!results[2]) return '';
-	return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
+botaoJoao.addEventListener("click", () => createCard());
